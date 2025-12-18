d1131faf2d42f6274d2fe379b85ecce8330d9ba4
solaimooon <118716931+solaimooon@users.noreply.github.com>
2025-12-18 23:43:53 +0330

    for debuging

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 CSRF_TRUSTED_ORIGINS = ['https://mjes.ir',
     "https://www.mjes.ir",]
 
